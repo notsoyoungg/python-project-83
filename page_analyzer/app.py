cab836e9a067f57b9113b3cb6757e61dab2da277
notsoyoung2 <p.sergey1997@mail.ru>
2023-03-11 14:53:31 +0600

    changes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -24,6 +24,35 @@ def get_conn():
     return psycopg2.connect(os.getenv("DATABASE_URL"))
 
 
+def select_from_db(*args, fetch='one'):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute(*args)
+    if fetch != 'one':
+        data = cur.fetchall()
+        cur.close()
+        conn.close()
+        return data
+    data = cur.fetchone()
+    cur.close()
+    conn.close()
+    return data
+
+
+def insert_to_db(query, params):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute(query, params)
+    try:
+        data = cur.fetchone()
+    except psycopg2.ProgrammingError:
+        data = ()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return data
+
+
 def normalize_url(url):
     parsed_url = urlparse(url)
     return parsed_url.scheme + '://' + parsed_url.netloc
@@ -68,33 +97,24 @@ def index():
 
 @app.route('/urls', methods=('GET', 'POST'))
 def urls():
-    conn = get_conn()
     if request.method == 'POST':
         url = request.form.get('url')
         if validators.url(url) and validators.length(url, max=255):
             normalized_url = normalize_url(url)
             try:
-                cur = conn.cursor()
-                cur.execute('''
+                data = insert_to_db('''
                             INSERT INTO urls(name, created_at)
                             VALUES (%s, %s)
                             RETURNING id;
                             ''',
                             (normalized_url, datetime.datetime.now()))
-                id = cur.fetchone()[0]
-                conn.commit()
-                cur.close()
-                conn.close()
+                id = data[0]
                 flash('Страница успешно добавлена', 'info')
                 return redirect(url_for('url', id=id))
             except psycopg2.errors.UniqueViolation:
-                conn.rollback()
-                cur = conn.cursor()
-                cur.execute('SELECT id FROM urls WHERE name = %s;',
-                            (normalized_url,))
-                id = cur.fetchone()[0]
-                cur.close()
-                conn.close()
+                data = select_from_db('SELECT id FROM urls WHERE name = %s;',
+                                      (normalized_url,))
+                id = data[0]
                 flash('Страница уже существует', 'info')
                 return redirect(url_for('url', id=id))
         flash('Некорректный URL', 'danger')
@@ -103,39 +123,27 @@ def urls():
                                url=url_for('index'),
                                messages=messages), 422
     if request.method == 'GET':
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM urls ORDER BY id DESC;')
-        sites = cur.fetchall()
-        print(sites)
-        cur.execute('''
-                    SELECT DISTINCT ON (url_id) created_at, status_code
-                    FROM url_checks
-                    ORDER BY url_id DESC, created_at DESC;
-                    ''')
-        checks = cur.fetchall()
-        cur.close()
-        conn.close()
-        print(checks)
+        sites = select_from_db('SELECT * FROM urls ORDER BY id DESC;', fetch='all')
+        checks = select_from_db('''
+                                SELECT DISTINCT ON (url_id) created_at, status_code
+                                FROM url_checks
+                                ORDER BY url_id DESC, created_at DESC;
+                                ''', fetch='all')
         return render_template('urls.html', sites=sites, checks=checks)
 
 
 @app.route('/urls/<id>', methods=('GET',))
 def url(id):
-    conn = get_conn()
-    cur = conn.cursor()
     messages = get_flashed_messages(with_categories=True)
-    conn.cursor()
-    cur.execute('SELECT * FROM urls WHERE id = %s;', (id,))
-    site = cur.fetchone()
-    cur.execute('''
-                SELECT * FROM url_checks
-                WHERE url_id = %s
-                ORDER BY id DESC;
-                ''',
-                (id,))
-    checks = cur.fetchall()
-    cur.close()
-    conn.close()
+    data = select_from_db('SELECT * FROM urls WHERE id = %s;', (id,))
+    site = data
+    checks = select_from_db('''
+                            SELECT * FROM url_checks
+                            WHERE url_id = %s
+                            ORDER BY id DESC;
+                            ''',
+                            (id,),
+                            fetch='all')
     return render_template('url_detail.html',
                            site=site,
                            checks=checks,
@@ -144,26 +152,18 @@ def url(id):
 
 @app.route('/urls/<id>/checks', methods=('POST',))
 def url_check(id):
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute('SELECT name FROM urls WHERE id = %s;', (id,))
-    url, = cur.fetchone()
-    print(url)
+    data = select_from_db('SELECT name FROM urls WHERE id = %s;', (id,))
+    url = data[0]
     try:
         data = get_info_about_site(url, id)
-        cur.execute('''
-                    INSERT INTO url_checks
-                    (url_id, status_code, h1, title, description, created_at)
-                    VALUES (%s, %s, %s, %s, %s, %s);
-                    ''',
-                    data)
-        conn.commit()
-        cur.close()
-        conn.close()
+        insert_to_db('''
+                     INSERT INTO url_checks
+                     (url_id, status_code, h1, title, description, created_at)
+                     VALUES (%s, %s, %s, %s, %s, %s);
+                     ''',
+                     data)
         flash('Страница успешно проверена', 'success')
         return redirect(url_for('url', id=id))
-    except (requests.exceptions.ConnectionError,
-            requests.exceptions.HTTPError) as e:
-        print(e)
+    except requests.exceptions.RequestException as e:
         flash('Произошла ошибка при проверке', 'danger')
         return redirect(url_for('url', id=id))
